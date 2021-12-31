6221e876f688ee73b245435c5588f111b1cfb406
Nikolas35 <96911732+Nikolas35@users.noreply.github.com>
2021-12-31 13:30:27 +0200

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010808080808080808080808080808010308080808080808080808080809080101080101010101010101010801010101010801010101010101010108010101010108010201010101010101080109080808080101010101010101010801080808080801010101010101010108010808080808010101010101010101080108080808080101010101010101010801080808080801010101010101010108010808080808010101010101010101080108080607080101010108080808080801080805040808080808080801010101010808080801010108080808080808080808080808`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . 2 2 
+. 2 2 2 2 2 2 2 2 2 . 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 . 2 2 2 2 2 
+. 2 . . . . . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 . . . . . . 2 . . . . 
+. . . . . . . 2 2 2 2 2 . . . . 
+2 2 2 . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.floorDark2,sprites.dungeon.stairLadder], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010808080808080808080808080808010308080808080808080808080809080101080101010101010101010801010101010801010101010101010108010101010108010201010101010101080109080808080101010101010101010801080808080801010101010101010108010808080808010101010101010101080108080808080101010101010101010801080808080801010101010101010108010808080808010101010101010101080108080607080101010108080808080801080805040808080808080801010101010808080801010108080808080808080808080808`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . 2 2 
+. 2 2 2 2 2 2 2 2 2 . 2 2 2 2 2 
+. 2 2 2 2 2 2 2 2 2 . 2 2 2 2 2 
+. 2 . . . . . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 2 . 2 2 2 . 2 . . . . 
+. 2 2 2 2 . . . . . . 2 . . . . 
+. . . . . . . 2 2 2 2 2 . . . . 
+2 2 2 . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.floorDark2,sprites.dungeon.stairLadder], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
